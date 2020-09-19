10e6bee06acdf9d9813e16d00220f40188700a1e
siolinm <siolinm@hotmail.com>
2020-09-19 11:14:00 -0300

    Shell pronto, falta fazer o relatório e o EP1

--- codigo/bccsh.c
+++ codigo/bccsh.c
@@ -12,81 +12,89 @@
 #include <readline/history.h>
 #include <readline/readline.h>
 
-#define SIZE_CUR_DIR 100 
+#define SIZE_CUR_DIR 100
 
-// Variáveis (globais?) usuário e diretório atual ====
+/* variáveis (globais?) usuário e diretório atual ==== */
 char cur_dir[SIZE_CUR_DIR]; 
 char *cur_user;
 
-/** Função read_command
+/** Função read_command(char **command, char **parameters)
  * Usa GNU readline e GNU history para ler um comando do terminal 
- * e retornar esse comando e os parâmetros indicados.
+ * e retornar esse comando em command e os parâmetros indicados em parameters.
  */ 
-void read_command(char **command, char **parameters) {
+void read_command(char **command, char **parameters){
     char prompt_line[500];
 
-    // Imprimindo o prompt
+    /* imprimindo o prompt */
     cur_user = getenv("USER");
     getcwd(cur_dir, sizeof(cur_dir));
     sprintf(prompt_line, "{%s@%s} ", cur_user, cur_dir);
 
-    // Lendo comando do shell
+    /* lendo comando do shell */
     *command = readline(prompt_line);
     add_history(*command);
 
-    // Retornando o comando e os parâmetros
+    /* retornando o comando e os parâmetros, reaproveitando a memória que já foi alocada por readline */
     int i = 0;
+    
+    if(*command != NULL){
+        while((*command)[i] != ' ' && (*command)[i] != '\0') { i++; }
 
-    while ((*command)[i] != ' ' && (*command)[i] != '\0') { i++; }
+        for(int j = 0; j < 20; parameters[j] = NULL, j++);
+        
+        parameters[0] = *command;
 
-    for (int j = 0; j < 20; parameters[j] = NULL, j++);
-
-    for (int j = 0; (*command)[i] != '\0'; i++) {
-        if ((*command)[i] == ' ') {
-            parameters[j] = *command+i+1;
-            (*command)[i] = '\0';
-            j++;
+        for(int j = 1; (*command)[i] != '\0'; i++){
+            if ((*command)[i] == ' ') {
+                parameters[j] = *command+i+1;
+                (*command)[i] = '\0';
+                j++;
+            }
         }
     }
 }
 
-void shell_control() {
-    // Comando e parâmetro lidos no terminal
+int main(int argc, char const *argv[]) {
+    /* comando e parâmetro lidos no terminal */
     char *command;
     char *parameters[20];
-    // Variável devolvida pelo método waitpid
+    /* variável devolvida pelo método waitpid */
     int status;
 
     while (1) {
         read_command(&command, parameters);
 
-        // ifzão dos syscall
-        if (!strcmp(command, "mkdir")) {
-           mkdir(parameters[0], S_IRWXU);
-        } else if (!strcmp(command, "exit")) {
+        /* if para syscalls */
+        if(command == NULL){
+            printf("\n");
             rl_clear_history();
             exit(0);
-        } else if (!strcmp(command, "kill")) {
-            // Tratar o comando e pegar os parâmetros e PID
-            pid_t pid = atoi(parameters[1]);
-            kill(pid, -atoi(parameters[0])); 
-        } else if (!strcmp(command, "ln") && !strcmp(parameters[0], "-s")) {
-           symlink(parameters[1], parameters[2]); 
+        }
+        else if(!strcmp(command, "mkdir")){
+           mkdir(parameters[1], S_IRWXU);
+        }
+        else if(!strcmp(command, "kill")){
+            /* tratar o comando e pegar os parâmetros e PID */
+            pid_t pid = atoi(parameters[2]);
+            kill(pid, -atoi(parameters[1]));
+        }
+        else if(!strcmp(command, "ln") && !strcmp(parameters[1], "-s")){            
+            symlink(parameters[2], parameters[3]); 
+        }
+        else{
+            if(fork() != 0){
+                /* código do processo pai */
+                waitpid(-1, &status, 0);
+            }
+            else{
+                /* código do processo filho */            
+                execve(command, parameters, NULL);
+                exit(0);
+            }
         }
 
-        if (fork() != 0) {
-            // Código do processo pai
-            waitpid(-1, &status, 0);
-        } else {
-            // Código do processo filho
-            execve(command, parameters, 0);
-        } 
         free(command);
     }
-}
-
-int main(int argc, char const *argv[]) {
-    shell_control();
 
     return 0;
 }
